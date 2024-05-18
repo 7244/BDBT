cf742133997e4bd27e95ac28a6a9c82b22242492
7244 <123005911+7244@users.noreply.github.com>
2024-05-18 18:47:34 +0300

    c typo

--- internal/PrepareAndInclude.h
+++ internal/PrepareAndInclude.h
@@ -11,8 +11,8 @@
 #if BDBT_set_declare_rest == 1
   #if defined(BDBT_set_lc)
     #define _BDBT_this This
-    #define _BDBT_fdec(rtype, name, ...) static rtype name(_P(t) *_BDBT_this, ##__VA_ARGS__)
-    #define _BDBT_fcall(name, ...) name(_BDBT_this, ##__VA_ARGS__)
+    #define _BDBT_fdec(rtype, name, ...) static rtype name(_P(t) *This, ##__VA_ARGS__)
+    #define _BDBT_fcall(name, ...) name(This, ##__VA_ARGS__)
   #elif defined(BDBT_set_lcpp)
     #define _BDBT_this this
     #define _BDBT_fdec(rtype, name, ...) rtype name(__VA_ARGS__)
